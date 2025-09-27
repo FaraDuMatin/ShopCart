669722cfa4ad3688e10dd46927a15db73f1df421
FaraDuMatin <118701688+FaraDuMatin@users.noreply.github.com>
2025-09-27 19:37:38 -0400

    Path and name errors

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ShopingCart/', // Replace with your repository name
+  base: '/ShopCart/', // Updated to match your GitHub repository name
   build: {
     outDir: 'dist',
   },
