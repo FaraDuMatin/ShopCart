38084dbb871f8a63e0fbbabef984f2dedbe8c88f
Farah Mohamed <118701688+FaraDuMatin@users.noreply.github.com>
2025-09-28 19:41:09 -0400

    Change Router from BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -10,7 +10,7 @@ import DarkVeil from "./components/Darkveil";
 function App() {
   return (
     <CartProvider>
-      <Router basename="/ShopCart">
+      <Router>
         <div className="App">
           <DarkVeil hueShift={24}/>
           <div className="app-content">
